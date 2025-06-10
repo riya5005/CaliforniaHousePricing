a0327ad74718fc3d316968aae49f34c02c61fd59
Riya <sharmariya50052@gmail.com>
2025-06-10 22:11:11 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import pandas as pd
 app=Flask(__name__)
 
 #load pickle
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scaler = pickle.load(open('scaler.pkl', 'rb'))  # ✅ Load your saved scaler
-
+# Load pickle files
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))  # ✅ This line is necessary
 
 @app.route('/')
 def home():
@@ -25,6 +25,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The house price prediction is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
